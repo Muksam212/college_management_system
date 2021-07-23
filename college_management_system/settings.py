07b4dc1ac4d24a1144878059d4b89021fdad6ba5
Muksam212 <muksamthebe10@gmail.com>
2021-07-23 09:41:41 +0545

    Updated STATIC_ROOT

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -31,8 +31,9 @@ import os
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+
 STATIC_URL='/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Application definition
 
